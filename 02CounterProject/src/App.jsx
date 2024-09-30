3398b1d2bae87e7df4ec6e8813dd2c3af76e7f0f
Ashu <ashutoshverma557@gmail.com>
2024-09-30 12:37:51 +0530

    update

--- 02CounterProject/src/App.jsx
+++ 02CounterProject/src/App.jsx
@@ -4,8 +4,22 @@ function App() {
   // let counter = 1;
   let[counter,setcounter] = useState(1)
   const addValue = () => {
-    // counter =counter+1;
-    setcounter(counter+1);
+      // setcounter(counter+1) if we want to increase the value from suppose from 4->7 the this method will not work then we need to do this via below approach
+    // setcounter(counter+1)
+    // setcounter(counter+1)
+    setcounter((prevcounter)=>{
+      return prevcounter = prevcounter+1;
+    })
+    setcounter((prevcounter)=>{
+      return prevcounter = prevcounter+1;
+    })
+    setcounter((prevcounter)=>{
+      return prevcounter = prevcounter+1;
+    })
+    setcounter((prevcounter)=>{
+      return prevcounter = prevcounter+1;
+    })
+  
     console.log(counter);
   }
   const removeValue = () => {
